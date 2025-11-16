c42774c955f579025dd14b918d522048f396943f
Herfando <herfando_91@yahoo.com>
2025-11-16 19:17:34 +0700

    create log in

--- src/components/container/Login.tsx
+++ src/components/container/Login.tsx
@@ -1,7 +1,11 @@
 export default function Login() {
   return (
     <section className='pr-11xl flex h-auto w-full items-center justify-center pt-217 pb-216 pl-34 md:px-520 md:pt-295 md:pb-298'>
-      <div className='h-419 w-324 bg-amber-300 md:h-431 md:w-400'></div>
+      <div className='h-419 w-324 bg-amber-300 md:h-431 md:w-400'>
+        <div>
+          <img src='../../../public/icons/01_logo company.svg' />
+        </div>
+      </div>
     </section>
   );
 }
