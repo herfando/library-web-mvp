7b58cfaf4a720ce8170ae09a5c1e6efe6d1e2238
Herfando <herfando_91@yahoo.com>
2025-11-18 13:35:17 +0700

    edit detail

--- src/components/container/Detail.tsx
+++ src/components/container/Detail.tsx
@@ -23,21 +23,26 @@ export default function Detail() {
       </div>
       {/* 2. Book's detail */}
       {/* Book's image */}
-      <div>
-        {/* outline */}
-        <div className='relative flex h-498 w-337 bg-[#E9EAEB]'>
-          {/* book's */}
-          <div>
-            <img
-              src='../../images/06_img dummy4 book detail.png'
-              alt='book title'
-              className='absolute top-1/2 left-1/2 h-482 w-321 -translate-x-1/2 -translate-y-1/2'
-            />
+      <div className='flex space-x-16'>
+        <div>
+          {/* outline */}
+          <div className='relative flex h-498 w-337 bg-[#E9EAEB]'>
+            {/* book's */}
+            <div>
+              <img
+                src='../../images/06_img dummy4 book detail.png'
+                alt='book title'
+                className='absolute top-1/2 left-1/2 h-482 w-321 -translate-x-1/2 -translate-y-1/2'
+              />
+            </div>
           </div>
         </div>
+        {/* Detail */}
+        <div className='bg-accent-green h-462 w-827 self-center'>
+          <h4>Business & Economics</h4>
+          <h2>The Psychology of Money</h2>
+        </div>
       </div>
-      {/* Detail */}
-      <div></div>
     </section>
   );
 }
