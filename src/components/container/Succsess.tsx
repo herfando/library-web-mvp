d3952a07354dfe3efd4cbd0e8913480fad405115
Herfando <herfando_91@yahoo.com>
2025-11-20 06:17:08 +0700

    edit success

--- src/components/container/Succsess.tsx
+++ src/components/container/Succsess.tsx
@@ -1,7 +1,11 @@
+import { Check } from 'lucide-react';
+
 export default function Sucsess() {
   return (
-    <section className='bg-accent-green top-1/2 mx-auto mt-190 md:mt-243'>
-      Sucsess<div>Sucsess</div>
+    <section className='mb-253.62 not-last-of-type:md:mb-368.62 top-1/2 mx-auto mt-190 flex h-auto w-full max-w-638 flex-col items-center justify-center bg-blue-200 md:mt-243'>
+      <div>
+        <Check />
+      </div>
     </section>
   );
 }
