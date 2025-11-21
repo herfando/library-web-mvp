a4a46ae25a7c96043cad60022b83fa5306b1d003
Herfando <herfando_91@yahoo.com>
2025-11-21 21:42:24 +0700

    created query

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,16 @@ import App from './App.tsx';
 import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import queryClient from './query/queryClient.ts';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </PersistGate>
     </Provider>
   </StrictMode>
