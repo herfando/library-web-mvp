6c6baad19a524096944ebb717eaeb3be16b386fa
Herfando <herfando_91@yahoo.com>
2025-11-21 16:13:48 +0700

    add zod to register

--- src/components/container/user/02_Register.tsx
+++ src/components/container/user/02_Register.tsx
@@ -7,23 +7,33 @@ import { useRegisterMutation } from '../../../redux/services/authApi';
 import { useAppDispatch } from '../../../redux/hooks/useAuth';
 import { setCredentials } from '../../../redux/slices/authSlice';
 import { toast } from 'react-hot-toast';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { registerSchema, type RegisterSchema } from '../../../validation/auth';
 
 export default function Register() {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const [register, { isLoading }] = useRegisterMutation();
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirm, setConfirm] = useState('');
+  const [registerUser, { isLoading }] = useRegisterMutation();
   const [show, setShow] = useState(false);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (password !== confirm) return toast.error('Password tidak sama!');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit = async (data: RegisterSchema) => {
     try {
-      const userData = await register({ name, email, password }).unwrap();
+      const userData = await registerUser({
+        name: data.name,
+        email: data.email,
+
+        password: data.password,
+      }).unwrap();
+
       dispatch(setCredentials(userData));
       toast.success('Register sukses!');
       navigate('/home');
@@ -48,21 +58,34 @@ export default function Register() {
         <p className='text-md text-\[\#414651\] mb-20 font-semibold whitespace-nowrap'>
           Create your account to start borrowing books.
         </p>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           {/* Name */}
           <div className='mb-16'>
             <div className='mb-2 text-sm font-bold'>Name</div>
-            <Input value={name} onChange={(e) => setName(e.target.value)} />
+            <Input {...register('name')} />
+            {errors.name && (
+              <p className='mt-1 text-sm text-red-500'>{errors.name.message}</p>
+            )}
           </div>
           {/* Email */}
           <div className='mb-16'>
             <div className='mb-2 text-sm font-bold'>Email</div>
-            <Input value={email} onChange={(e) => setEmail(e.target.value)} />
+            <Input {...register('email')} />
+            {errors.email && (
+              <p className='mt-1 text-sm text-red-500'>
+                {errors.email.message}
+              </p>
+            )}
           </div>
           {/* Nomor Handphone */}
           <div className='mb-16'>
             <div className='mb-2 text-sm font-bold'>Nomor Handphone</div>
-            <Input placeholder='081234567890' />
+            <Input placeholder='081234567890' {...register('phone')} />
+            {errors.phone && (
+              <p className='mt-1 text-sm text-red-500'>
+                {errors.phone.message}
+              </p>
+            )}
           </div>
           {/* Password */}
           <div className='mb-16'>
@@ -71,14 +94,18 @@ export default function Register() {
               <Input
                 type={show ? 'text' : 'password'}
                 placeholder='johndoe123'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                {...register('password')}
               />
               <Eye
                 onClick={() => setShow(!show)}
                 className='absolute top-1/2 right-14 -translate-y-1/2 cursor-pointer text-[#0A0D12]'
               />
             </div>
+            {errors.password && (
+              <p className='mt-1 text-sm text-red-500'>
+                {errors.password.message}
+              </p>
+            )}
           </div>
           {/* Confirm Password*/}
           <div className='mb-16'>
@@ -87,14 +114,18 @@ export default function Register() {
               <Input
                 type={show ? 'text' : 'password'}
                 placeholder='johndoe123'
-                value={confirm}
-                onChange={(e) => setConfirm(e.target.value)}
+                {...register('confirm')}
               />
               <Eye
                 onClick={() => setShow(!show)}
                 className='absolute top-1/2 right-14 -translate-y-1/2 cursor-pointer text-[#0A0D12]'
               />
             </div>
+            {errors.confirm && (
+              <p className='mt-1 text-sm text-red-500'>
+                {errors.confirm.message}
+              </p>
+            )}
           </div>
           {/* Button */}
           <Button
