2633520ef785bd91cbe8f9ed4a4549927112aedb
Herfando <herfando_91@yahoo.com>
2025-11-29 12:38:05 +0700

    edit navbar

--- src/components/ui/dropDown.tsx
+++ src/components/ui/dropDown.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { X } from 'lucide-react';
+import { ChevronDown } from 'lucide-react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -14,49 +14,50 @@ export default function DropDown({}) {
   };
 
   return (
-    <div className='relative block md:hidden'>
+    <div className='relative'>
       {/* Navbar atas */}
       <nav className='flex items-center justify-between'>
         {/* Tombol hamburger */}
         <button
           onClick={handleToggleMenu}
-          className='z-20 rounded-lg'
+          className='z-1 rounded-lg hover:cursor-pointer'
           aria-label='Toggle menu'
         >
-          {isOpen ? (
-            // Ikon X saat terbuka
-            <X
-              size={28}
-              className='text-black hover:cursor-pointer dark:text-white'
-            />
-          ) : (
-            // Garis 3 manual
-            <div className='grid h-24 w-24 -translate-x-4 justify-items-end gap-6 p-3 hover:cursor-pointer'>
-              <span className='h-2 w-24 bg-black'></span>
-              <span className='h-2 w-24 bg-black'></span>
-              <span className='h-2 w-24 bg-black'></span>
-            </div>
-          )}
+          <ChevronDown />
         </button>
       </nav>
 
       {/* Menu mobile muncul setelah diklik */}
-      <div
-        className={`fixed top-0 left-0 z-1 h-136 w-full transform bg-white text-black transition-transform duration-1000 ease-in-out ${
-          isOpen
-            ? 'translate-y-0' // /* TRANSLATE HERE: menu muncul */
-            : '-translate-y-full' // /* TRANSLATE HERE: menu geser keluar */
-        }`}
-      >
-        <div className='custom-container flex h-136 w-full flex-col justify-center gap-12 text-black'>
-          <h3
-            onClick={() => navigate('/profil')}
-            className='md:text-md text-sm font-semibold'
-          >
-            Profile
-          </h3>
+      {isOpen && (
+        <div className='fixed top-64 right-0 z-1 h-200 w-full rounded-b-4xl bg-white text-black shadow-sm md:top-80 md:right-50 md:w-184'>
+          <div className='custom-container flex h-200 w-full flex-col justify-between gap-12 p-16 text-black'>
+            <p
+              onClick={() => navigate('/profil')}
+              className='md:text-md text-sm font-semibold hover:text-[#EE1D52]'
+            >
+              Profile
+            </p>
+            <p
+              onClick={() => navigate('/profil')}
+              className='md:text-md text-sm font-semibold hover:text-[#EE1D52]'
+            >
+              Borrowed List
+            </p>
+            <p
+              onClick={() => navigate('/profil')}
+              className='md:text-md text-sm font-semibold hover:text-[#EE1D52]'
+            >
+              Reviews
+            </p>
+            <p
+              onClick={() => navigate('/profil')}
+              className='md:text-md text-sm font-semibold hover:text-[#EE1D52]'
+            >
+              Logout
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
